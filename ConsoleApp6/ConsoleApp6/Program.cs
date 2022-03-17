69d172a80b872d5877f55d89b24cc6c333f64654
GustavoYM01 <gumatimoto@hotmail.com>
2022-03-17 13:33:47 -0300

    Modified files

--- ConsoleApp6/ConsoleApp6/Program.cs
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -11,15 +11,17 @@ namespace ConsoleApp6
 
             Retangulo r = new Retangulo();
 
-            Console.WriteLine("Informe a base do " +
+            Console.Write("Informe a base do " +
                 "retângulo: ");
             r.Base = double.Parse(Console.ReadLine());
 
-            Console.WriteLine("Informe a altura do " +
+            Console.Write("Informe a altura do " +
                 "retângulo: ");
             r.Altura = double.Parse(Console.ReadLine());
 
             double areaR = r.CalculaAreaRetangulo(r.Base, r.Altura);
+
+            Console.WriteLine();
 
             Console.WriteLine("Área do retângulo: " +
                 areaR.ToString("F2", CI));
